d5b441de43a6c9faad19e073c1b60c004801be32
Wai Pai Lee <pailee.wai@gmail.com>
2018-01-17 22:13:20 +0800

    change rootpath name

--- src/starter/CommonUtilities.cpp
+++ src/starter/CommonUtilities.cpp
@@ -1,7 +1,7 @@
 #include "CommonUtilities.h"
 
 std::string CommonUtilities::getFullPath(std::string relativePath) {
-	std::string path = ROOT_PATH;
+	std::string path = OGLSP_ROOT_PATH;
 	std::string result = path + "/" + relativePath;
 
 	return result;
